50a3bdcc0fc7cbb62e80e69c394591e5f38064ac
hfeng101 <1343226950@qq.com>
2024-11-25 20:04:15 +0800

    update

--- TestCRD/main.go
+++ TestCRD/main.go
@@ -17,8 +17,14 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	"os"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
@@ -77,9 +83,86 @@ func main() {
 	}
 	// +kubebuilder:scaffold:builder
 
+	// 1.构建context
+	//ctx := context.CancelFunc(context.Background)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// 2.select等待选举结果
+	c := leaderElection(ctx, mgr.GetClient())
+	select {
+	case ctx:
+		setupLog.Info("exit ")
+		os.Exit(0)
+	case c:
+		mgr.Start(ctrl.SetupSignalHandler())
+	default:
+		setupLog.Info("Waiting for leader election!")
+	}
+
+	// 3.销毁退出
+
+
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+
+
+
+}
+
+// 多副本选举，选举可以通过configmap/lease/endpoint三种方式实现，k8s1.18之后，删除了configmap的方式
+func leaderElection(ctx context.Context, k8sClient client.Client)<-chan{
+	leaseLockName := ""
+	leaseLockNamespace := ""
+
+	resourcelock.LeaderElectionRecordToLeaseSpec()
+	resourcelock.LeaseSpecToLeaderElectionRecord()
+
+	leaseLock := &resourcelock.LeaseLock{
+		LeaseMeta: v1.ObjectMeta{
+			Name: leaseLockName,
+			Namespace: leaseLockNamespace,
+		},
+		Client: k8sClient,
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity: "TestCRD",
+			EventRecorder: nil,
+		},
+	}
+
+	//跑选举库做选举
+	leaderelection.RunOrDie(ctx,
+		leaderelection.LeaderElectionConfig{
+			Lock: leaseLock,
+
+			LeaseDuration: 60*time.Second,
+			RenewDeadline: 15*time.Second,
+			RetryPeriod: 5*time.Second,
+
+			ReleaseOnCancel: true,
+
+			Callbacks: leaderelection.LeaderCallbacks{
+				OnNewLeader: func(identity string){
+					setupLog.Info("TestCRD")
+				},
+				OnStartedLeading: func(ctx context.Context){
+					setupLog.Info("TestCRD")
+				},
+				OnStoppedLeading: func(){
+					setupLog.Info("TestCRD")
+				},
+			},
+		})
+
+	//获取lease，确认lease是否被占用
+
+	//设置lease，确认当选leader
+
+	//当选leader，加载配置及缓存，进入主程序
+
+	//释放leader
+
+	return c(<-chan)
 }
